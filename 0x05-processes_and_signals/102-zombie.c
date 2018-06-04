ef1396f288fd3a9bdcbacb058ff9ec0c9713b14a
Dkazem91 <Dkazemian@gmail.com>
2018-06-04 14:37:00 +0000

    added zombies

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * infinite_while - sleeps forever
+ * Return: 1
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
+ * main - creates 5 zombies
+ * Return: always 1
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (fork() == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(1);
+		}
+	}
+	infinite_while();
+}
