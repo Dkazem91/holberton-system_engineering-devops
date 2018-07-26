df8eabdaca425b392d2bc979ccd16bcfed3218f9
Dkazem91 <Dkazemian@gmail.com>
2018-07-26 12:52:11 +0000

    first puppet test

---
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#creates a file
+File{'/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
