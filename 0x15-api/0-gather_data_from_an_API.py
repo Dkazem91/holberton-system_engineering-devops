fa7d9f6dda60e29ae1cb363c60bc1250c89e10bf
Dkazem91 <Dkazemian@gmail.com>
2018-08-27 19:48:33 +0000

    put it in a function

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 # gets an api
-import requests
-from sys import argv
-
-
-if __name__ == "__main__" and argv[1].isdigit():
+def todo(userid):
     """doc stringed"""
     name = requests.get('https://jsonplaceholder.typicode.com/users/'
-                        + argv[1]).json().get('name')
+                        + userid).json().get('name')
     tasks = requests.get('https://jsonplaceholder.typicode.com/users/'
-                         + argv[1] + '/todos').json()
-    tasksDone = ['\t {}\n'.format(dic.get('title')) for dic in tasks
-                 if dic.get('completed')]
+                         + userid + '/todos').json()
+    tasksDone =['\t {}\n'.format(dic.get('title')) for dic in tasks
+                if dic.get('completed')]
     if name and tasks:
         print("Employee {} is done with tasks({}/{}):".format
               (name, len(tasksDone), len(tasks)))
         print(''.join(tasksDone), end='')
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    if len(argv) == 2:
+        todo(argv[1])
