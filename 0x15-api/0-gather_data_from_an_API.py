34919365cd658e0a3c51ffa884fafde6396b5627
Dkazem91 <Dkazemian@gmail.com>
2018-08-27 19:10:18 +0000

    done with 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# gets an api
+import requests
+from sys import argv
+
+tasks = requests.get('https://jsonplaceholder.typicode.com/users/'
+                     + argv[1] + '/todos').json()
+name = requests.get('https://jsonplaceholder.typicode.com/users/'
+                    + argv[1]).json().get('name')
+tasksDone = {'\t {}\n'.format(dic.get('title')) for dic in tasks
+             if dic.get('completed')}
+print("Employee {} is done with tasks({}/{}):".format
+      (name, len(tasksDone), len(tasks)))
+print(''.join(tasksDone))
