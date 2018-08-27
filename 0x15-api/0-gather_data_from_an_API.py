70ede13a5a9831e3b05988438f634c48e03bf418
Dkazem91 <Dkazemian@gmail.com>
2018-08-27 19:55:58 +0000

    added integer cast

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-# gets an api
+"""gets api"""
+import requests
+from sys import argv
+
+
 def todo(userid):
     """doc stringed"""
-    name = requests.get('https://jsonplaceholder.typicode.com/users/'
-                        + userid).json().get('name')
-    tasks = requests.get('https://jsonplaceholder.typicode.com/users/'
-                         + userid + '/todos').json()
-    tasksDone =['\t {}\n'.format(dic.get('title')) for dic in tasks
-                if dic.get('completed')]
+    name = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(
+            userid)).json().get('name')
+    tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+            userid)).json()
+    tasksDone = ['\t {}\n'.format(dic.get('title')) for dic in tasks
+                 if dic.get('completed')]
     if name and tasks:
         print("Employee {} is done with tasks({}/{}):".format
               (name, len(tasksDone), len(tasks)))
         print(''.join(tasksDone), end='')
 
+
 if __name__ == "__main__":
-    import requests
-    from sys import argv
     if len(argv) == 2:
-        todo(argv[1])
+        todo(int(argv[1]))
