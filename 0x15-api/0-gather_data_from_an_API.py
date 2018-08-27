fab6c1729ddee3edaecb50b259f1053f3a46dacb
Dkazem91 <Dkazemian@gmail.com>
2018-08-27 19:16:27 +0000

    if name main added

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,12 +3,14 @@
 import requests
 from sys import argv
 
-tasks = requests.get('https://jsonplaceholder.typicode.com/users/'
-                     + argv[1] + '/todos').json()
-name = requests.get('https://jsonplaceholder.typicode.com/users/'
-                    + argv[1]).json().get('name')
-tasksDone = {'\t {}\n'.format(dic.get('title')) for dic in tasks
-             if dic.get('completed')}
-print("Employee {} is done with tasks({}/{}):".format
-      (name, len(tasksDone), len(tasks)))
-print(''.join(tasksDone))
+
+if __name__ == "__main__":
+    tasks = requests.get('https://jsonplaceholder.typicode.com/users/'
+                         + argv[1] + '/todos').json()
+    name = requests.get('https://jsonplaceholder.typicode.com/users/'
+                        + argv[1]).json().get('name')
+    tasksDone = {'\t {}\n'.format(dic.get('title')) for dic in tasks
+                 if dic.get('completed')}
+    print("Employee {} is done with tasks({}/{}):".format
+          (name, len(tasksDone), len(tasks)))
+    print(''.join(tasksDone))
