5656b8702133573a8585738a848e605e84018d04
Dkazem91 <Dkazemian@gmail.com>
2018-08-27 19:38:58 +0000

    added null check

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,12 +6,13 @@ from sys import argv
 
 if __name__ == "__main__" and argv[1].isdigit():
     """doc stringed"""
-    tasks = requests.get('https://jsonplaceholder.typicode.com/users/'
-                         + argv[1] + '/todos').json()
     name = requests.get('https://jsonplaceholder.typicode.com/users/'
                         + argv[1]).json().get('name')
+    tasks = requests.get('https://jsonplaceholder.typicode.com/users/'
+                         + argv[1] + '/todos').json()
     tasksDone = ['\t {}\n'.format(dic.get('title')) for dic in tasks
                  if dic.get('completed')]
-    print("Employee {} is done with tasks({}/{}):".format
-          (name, len(tasksDone), len(tasks)))
-    print(''.join(tasksDone), end='')
+    if name and tasks:
+        print("Employee {} is done with tasks({}/{}):".format
+              (name, len(tasksDone), len(tasks)))
+        print(''.join(tasksDone), end='')
