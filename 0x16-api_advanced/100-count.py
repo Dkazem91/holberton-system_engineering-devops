edc768c964e43d6bb1efcb595af8b1ff37136e52
Dkazem91 <Dkazemian@gmail.com>
2018-08-30 21:14:53 +0000

    added printline checks

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -14,9 +14,12 @@ def count_all(hotlist, word_list):
         for word in words:
             if count_dic.get(word) is not None:
                 count_dic[word] += 1
+
     for key in sorted(count_dic, key=count_dic.get, reverse=True):
         if count_dic.get(key):
             print("{}: {}".format(key, count_dic[key]))
+    if set(count_dic.values()) == {0}:
+        print()
 
 
 def count_words(subreddit, word_list):
@@ -24,18 +27,24 @@ def count_words(subreddit, word_list):
     global after
     """subs"""
     head = {'User-Agent': 'Dan Kazam'}
-    if after:
-        count = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
-            subreddit, after), headers=head).json().get('data')
-    else:
-        count = get('https://www.reddit.com/r/{}/hot.json'.format(
-            subreddit), headers=head).json().get('data')
-    hotlist += [dic.get('data').get('title').lower()
-                for dic in count.get('children')]
-    after = count.get('after')
-    if after:
-        return count_words(subreddit, word_list)
-    return count_all(hotlist, word_list)
+    try:
+        if after:
+            count = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
+                subreddit, after), headers=head,
+                        allow_redirects=False).json().get('data')
+        else:
+            count = get('https://www.reddit.com/r/{}/hot.json'.format(
+                subreddit), headers=head,
+                        allow_redirects=False).json().get('data')
+        hotlist += [dic.get('data').get('title').lower()
+                    for dic in count.get('children')]
+        after = count.get('after')
+        if after:
+            return count_words(subreddit, word_list)
+        else:
+            return count_all(hotlist, word_list)
+    except:
+        print()
 
 
 if __name__ == "__main__":
