800351c0d2701b5521c8b476915640323fe2a713
Dkazem91 <Dkazemian@gmail.com>
2018-08-30 21:30:11 +0000

    added 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -8,7 +8,7 @@ after = None
 
 
 def count_all(hotlist, word_list):
-    count_dic = {word: 0 for word in word_list}
+    count_dic = {word.lower(): 0 for word in word_list}
     for title in hotlist:
         words = title.split(' ')
         for word in words:
@@ -17,9 +17,9 @@ def count_all(hotlist, word_list):
 
     for key in sorted(count_dic, key=count_dic.get, reverse=True):
         if count_dic.get(key):
-            print("{}: {}".format(key, count_dic[key]))
-    if set(count_dic.values()) == {0}:
-        print()
+            for thing in word_list:
+                if key == thing.lower():
+                    print("{}: {}".format(thing, count_dic[key]))
 
 
 def count_words(subreddit, word_list):
@@ -27,25 +27,19 @@ def count_words(subreddit, word_list):
     global after
     """subs"""
     head = {'User-Agent': 'Dan Kazam'}
-    try:
-        if after:
-            count = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
-                subreddit, after), headers=head,
-                        allow_redirects=False).json().get('data')
-        else:
-            count = get('https://www.reddit.com/r/{}/hot.json'.format(
-                subreddit), headers=head,
-                        allow_redirects=False).json().get('data')
-        hotlist += [dic.get('data').get('title').lower()
-                    for dic in count.get('children')]
-        after = count.get('after')
-        if after:
-            return count_words(subreddit, word_list)
-        else:
-            return count_all(hotlist, word_list)
-    except:
-        print()
+    if after:
+        count = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
+            subreddit, after), headers=head).json().get('data')
+    else:
+        count = get('https://www.reddit.com/r/{}/hot.json'.format(
+            subreddit), headers=head).json().get('data')
+    hotlist += [dic.get('data').get('title').lower()
+                for dic in count.get('children')]
+    after = count.get('after')
+    if after:
+        return count_words(subreddit, word_list)
+    return count_all(hotlist, word_list)
 
 
 if __name__ == "__main__":
-    count_words(argv[1], argv[2].lower().split(' '))
+    count_words(argv[1], argv[2].split(' '))
