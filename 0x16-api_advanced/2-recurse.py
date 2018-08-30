17c1b211c2e20b34cd5bfc52bdca5472ef36118a
Dkazem91 <Dkazemian@gmail.com>
2018-08-30 19:18:27 +0000

    recursed

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,16 +7,20 @@ from sys import argv
 def recurse(subreddit, hotlist=[], after=None):
     """subs"""
     head = {'User-Agent': 'Dan Kazam'}
-    if after:
-        count = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
-            subreddit, after), headers=head).json().get('data')
-    else:
-        count = get('https://www.reddit.com/r/{}/hot.json'.format(
-            subreddit), headers=head).json().get('data')
-    hotlist += [dic.get('data').get('title') for dic in count.get('children')]
-    if count.get('after'):
-        return recurse(subreddit, hotlist, after=count.get('after'))
-    return hotlist
+    try:
+        if after:
+            count = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
+                subreddit, after), headers=head).json().get('data')
+        else:
+            count = get('https://www.reddit.com/r/{}/hot.json'.format(
+                subreddit), headers=head).json().get('data')
+        hotlist += [dic.get('data').get('title')
+                    for dic in count.get('children')]
+        if count.get('after'):
+            return recurse(subreddit, hotlist, after=count.get('after'))
+        return hotlist
+    except:
+        return None
 
 
 if __name__ == "__main__":
