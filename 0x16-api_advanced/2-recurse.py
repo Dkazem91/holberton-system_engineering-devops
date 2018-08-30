4c6c8972c3b232748b0339b792e69f1632388214
Dkazem91 <Dkazemian@gmail.com>
2018-08-30 18:17:15 +0000

    added new stuff

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,8 +3,11 @@
 from requests import get
 from sys import argv
 
+def paginate(Next):
+    if not Next:
+        return
 
-def top_ten(subreddit):
+def recurse(subreddit, after):
     """subs"""
     head = {'User-Agent': 'Dan Kazam'}
     count = get('https://www.reddit.com/r/{}/hot.json?count=10'.format(
