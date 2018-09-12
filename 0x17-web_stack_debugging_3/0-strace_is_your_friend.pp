1e44817f83541ff2d69df6f007f92c0b28a8e44c
Dkazem91 <Dkazemian@gmail.com>
2018-09-12 20:29:53 +0000

    added onlyif

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # fixes a file
 exec {'replaces wrong php filetype':
   command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
-  path => '/bin/:/sbin/:/usr/bin/:/usr/sbin/'
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+  onlyif  => 'test -f /var/www/html/wp-settings.php'
 }
