3f75a42fc0c3145e9e1ec48f6e846073f7de1a04
Dkazem91 <Dkazemian@gmail.com>
2018-09-17 18:05:31 +0000

    updated description and if check

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fixes a file
-exec {'replaces wrong php filetype':
+exec {'sets file limite for nginx':
   command => 'sed -i "s/15/2000/g" /etc/default/nginx',
   path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
-  onlyif  => 'test -f /var/www/html/wp-settings.php'
+  onlyif  => 'test -f /etc/default/nginx'
 }
